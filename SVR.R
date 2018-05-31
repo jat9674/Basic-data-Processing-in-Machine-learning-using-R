e3d89ae341a71c663c7f5687a1e2eb76d9ebd4d4
rajaram jat <iit2016046@iiita.ac.in>
2018-05-31 16:22:04 +0530

    Add files via upload

---
+++ SVR.R
@@ -0,0 +1,14 @@
+dataset=read.csv('Position_Salaries.csv')
+dataset=dataset[2:3]
+#install.packages('e1071')
+library(e1071)
+regressor=svm(formula=Salary~.,data=dataset,type='eps-regression')
+y_pred=predict(regressor,data.frame(Level=6.5))
+#visualising svr graph
+library(ggplot2)
+ggplot()+
+  geom_point(aes(x = dataset$Level,  y =dataset$Salary),colour='red')+
+  geom_line(aes( x = dataset$Level, y = predict(regressor, newdata = dataset)),colour='blue')+
+  ggtitle('Truth vs BLuff')+
+  xlab('Level')+
+  ylab('salary')
